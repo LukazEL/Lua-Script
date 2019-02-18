f06ce3a1f3b283b7a7a2ad57aaeec06991d91520
LukazEL <44091038+LukazEL@users.noreply.github.com>
2019-02-18 13:22:36 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -363,7 +363,7 @@ callbacks.Register("Draw", "SkyBox", SkyBox)
 local vis_main = gui.Reference('SETTINGS', "Miscellaneous")
 local box = gui.Groupbox( vis_main, "Extra Stuff", 0, 380, 213, 170 );
 local FullbrightCheckbox = gui.Checkbox(box, "lua_fullbright", "Full Brightness", 0 );
--- local RainbowMenu = gui.Checkbox(box, "lua_rainbowmenu", "Rainbow Menu", 0 );
+-- rainbow curenttly disable local RainbowMenu = gui.Checkbox(box, "lua_rainbowmenu", "Rainbow Menu", 0 );
 local AWMetallicHitsound = gui.Checkbox(box, "lua_metallichitsound", "Metallic Hitsound", 0 );
 local DamageSay = gui.Checkbox(box, "lua_damagesay", "Damage Log", 0 );
 local K_O_L_H = gui.Checkbox(box, "lua_knifelefthand", "Knife On Left Hand", 0)
